cd9f189669364e130eb66fa0f4454bd7ba23a8e3
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-11-12 10:23:57 -0800

    Made minor UI update
    Posts now display as rudimentary cards on the HomePage

--- src/Home/PostList.js
+++ src/Home/PostList.js
@@ -15,11 +15,13 @@ import { Link } from 'react-router-dom';
 function PostList({ titles }) {
 
   const postsTitlesCards = titles.map(title => (
-    <div className="card-body" key={title.id}>
+    <div className="card col-md-6 offset-md-3" key={title.id}>
+      <div className='card-body'>
       <h5 className="card-title">
         <Link to={`/${title.id}`} id={title.id}>{title.title} </Link>
       </h5>
       <p className="card-text">{title.description} </p>
+      </div>
     </div>
   ))
 
